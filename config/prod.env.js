2e7b41592d98a4ddf06bfe0a98da2890e23418f5
安牛 <zhenhua.xu@kongapi.com>
2019-09-18 19:37:08 +0800

    修改测试环境地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://admin-api.caishuiyu.com"'
+  BASE_API: '"http://tax-caishui-crm.int.anniu-inc.com"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  BASE_API: '"http://tax-caishui-admin.int.anniu-inc.com"'
+  BASE_API: '"http://tax-caishui-crm.int.anniu-inc.com"'
 }
