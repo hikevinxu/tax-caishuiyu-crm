b7012a6b7e2a56410734c99bcbae7d031adf4268
安牛 <zhenhua.xu@kongapi.com>
2019-10-16 12:33:46 +0800

    提交测试

--- src/views/customer/review.vue
+++ src/views/customer/review.vue
@@ -146,6 +146,11 @@ export default {
   },
   methods: {
     getList() {
+      for(let i=0;i<this.listData.length;i++) {
+        window.clearInterval(this.listData[i].timer)
+        this.listData[i].timer = null
+        this.listData[i].time = 0
+      }
       this.listLoading = true
       let params = {}
       for(let key  in this.listQuery){
@@ -157,10 +162,6 @@ export default {
         if(res.code == 0){
           this.listData = res.data.items
           for(let i=0;i<this.listData.length;i++) {
-            if (this.listData[i].timer) {
-              clearInterval(this.listData[i].timer)
-              this.listData[i].timer = ''
-            }
             if(this.listData[i].status != 4) {
               this.listData[i].time = parseInt(new Date(this.listData[i].expireTime).getTime()/1000 - new Date().getTime()/1000)
               this.listData[i].timer = setInterval(() => {
@@ -170,7 +171,6 @@ export default {
                   this.listData[i].status = 4
                   clearInterval(this.listData[i].timer)
                 }
-                this.$forceUpdate()
               },1000)
             }
             if (this.listData[i].extra && this.listData[i].extra != '') {
--- src/views/customer/reviewTask.vue
+++ src/views/customer/reviewTask.vue
@@ -148,6 +148,11 @@ export default {
   },
   methods: {
     getList() {
+      for(let i=0;i<this.listData.length;i++) {
+        window.clearInterval(this.listData[i].timer)
+        this.listData[i].timer = null
+        this.listData[i].time = 0
+      }
       this.listLoading = true
       let params = {}
       for(let key  in this.listQuery){
